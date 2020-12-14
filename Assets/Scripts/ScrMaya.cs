c6bce879d826c1e2edac8e7759491821d2ea1080
Pamoan10 <moretaap@campusemav.com>
2020-12-14 19:40:29 +0100

    Classe 14/12/2020
    Creació de noves carpetes, script personatge i importació de material

---
+++ Assets/Scripts/ScrMaya.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrMaya : MonoBehaviour
+{
+    [SerializeField]
+    float forsa = 3;
+    float x, y; //variables per guardar lectura dels cursors de forma global
+
+    //Vull definir un objecte del tipus Rigidbody anomenat rb (variable), val 0. Per accedir al component RigidBody:
+    Rigidbody2D rb;
+    //ScrPickup scrP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //ara rb apunta al component rigidBody del player AIXÒ ÉS BÀSIC
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal"); //llegir moviment horizontal
+        y = Input.GetAxis("Vertical"); //llegir moviment vertical
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(x * forsa, y * forsa));
+    }
+}
