9eff45801a5757437fc5640178991f67fdf19127
Pamoan10 <moretaap@campusemav.com>
2020-12-14 22:15:32 +0100

    Classe 14/12/2020 versió 2
    Animació del personatge principal

--- Assets/Scripts/ScrMaya.cs
+++ Assets/Scripts/ScrMaya.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ScrMaya : MonoBehaviour
 {
     [SerializeField]
-    float forsa = 3;
-    float x, y; //variables per guardar lectura dels cursors de forma global
+    float velocitat = 1f;
+    [SerializeField]
+    Animator animator; //permet
+
+    Vector2 moviment;
 
     //Vull definir un objecte del tipus Rigidbody anomenat rb (variable), val 0. Per accedir al component RigidBody:
     Rigidbody2D rb;
+    public 
     //ScrPickup scrP;
 
     // Start is called before the first frame update
@@ -18,14 +22,23 @@ public class ScrMaya : MonoBehaviour
         rb = GetComponent<Rigidbody2D>(); //ara rb apunta al component rigidBody del player AIXÒ ÉS BÀSIC
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal"); //llegir moviment horizontal
-        y = Input.GetAxis("Vertical"); //llegir moviment vertical
+        moviment.x = Input.GetAxisRaw("Horizontal");
+        moviment.y = Input.GetAxisRaw("Vertical");
+
+        if (moviment != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", moviment.x);
+            animator.SetFloat("Vertical", moviment.y);
+        }
+
+        animator.SetFloat("Velocitat", moviment.sqrMagnitude);
     }
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector2(x * forsa, y * forsa));
+        rb.MovePosition(rb.position + moviment * velocitat * Time.fixedDeltaTime);
     }
 }
