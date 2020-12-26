e738fc5f7b91ac7cdf4840d8f3e0eb51432511ee
Pamoan10 <moretaap@campusemav.com>
2020-12-27 00:54:38 +0100

    Classe 27/12/2020
    Implementat un menú principal

--- Assets/Scripts/ScrControlGame.cs
+++ Assets/Scripts/ScrControlGame.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
 
 public class ScrControlGame : MonoBehaviour
 {
     // Start is called before the first frame update
+    string[] escenes = { "Intro", "Nivell 1", "Nivell 2", "Nivell 3" }; // creo un array per organitzar els diferents nivells
+    static int level = 0; 
     void Start()
     {
         
@@ -13,6 +17,29 @@ public class ScrControlGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ControlEntradaUsuari();
+    }
+    void ControlEntradaUsuari()
+    {
+        if (Input.GetKeyDown(KeyCode.J)) Nivell1();
+        //if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.W)) Web();
+        if (Input.GetKeyDown(KeyCode.S)) Sortir();
+    }
+    public void Nivell1()
+    {
+        SceneManager.LoadScene(escenes[level + 1]); //em permet accedir al nivell 1 mitjançant el botó del menú
+    }
+    public void Controls()
+    {
+
+    }
+    public void Web()
+    {
+        Application.OpenURL("https://twitter.com/Pamoan10");
+    }
+    public void Sortir()
+    {
+        Application.Quit();
     }
 }
