b26c956bbda33990658969ebd714499caa1354f1
Pamoan10 <moretaap@campusemav.com>
2020-12-27 14:00:34 +0100

    Classe 27/12/2020 versió 3
    Intro creada, intro 2 creada, transicions creades i accedeix al menú principal

--- Assets/Scripts/ScrControlGame.cs
+++ Assets/Scripts/ScrControlGame.cs
@@ -7,11 +7,16 @@ using UnityEngine;
 public class ScrControlGame : MonoBehaviour
 {
     // Start is called before the first frame update
-    string[] escenes = { "Intro", "Nivell 1", "Nivell 2", "Nivell 3" }; // creo un array per organitzar els diferents nivells
-    static int level = 0; 
+    
+    [SerializeField]
+    Animator transicio;
+    [SerializeField]
+    float tempsTransicio = 1f;
+    [SerializeField]
+    float tempsEspera = 38f;
     void Start()
     {
-        
+        StartCoroutine(esperaIntro());
     }
 
     // Update is called once per frame
@@ -21,14 +26,32 @@ public class ScrControlGame : MonoBehaviour
     }
     void ControlEntradaUsuari()
     {
+        if (Input.GetKeyDown(KeyCode.Return)) CarregarNivell(); //per passar de la intro al menú
         if (Input.GetKeyDown(KeyCode.J)) Nivell1();
         //if (Input.GetKeyDown(KeyCode.C))
         if (Input.GetKeyDown(KeyCode.W)) Web();
         if (Input.GetKeyDown(KeyCode.S)) Sortir();
+        
+    }
+    public void CarregarNivell()
+    {
+        StartCoroutine(CarregaNivell(SceneManager.GetActiveScene().buildIndex + 1)); //Quan es premi la tecla Enter, passarà al següent nivell
+    }
+    IEnumerator CarregaNivell(int levelIndex) //no vull que canvii d'escena directament, per això creo que Coroutine que permetrà veure la fosa en negre que he creat
+    {
+        transicio.SetTrigger("Start");
+        yield return new WaitForSeconds(tempsTransicio);
+        SceneManager.LoadScene(levelIndex);
+    }
+    IEnumerator esperaIntro()
+    {
+        yield return new WaitForSeconds(tempsEspera);
+        SceneManager.LoadScene(1);
+        
     }
     public void Nivell1()
     {
-        SceneManager.LoadScene(escenes[level + 1]); //em permet accedir al nivell 1 mitjançant el botó del menú
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //em permet accedir al nivell 1 mitjançant el botó del menú
     }
     public void Controls()
     {
@@ -36,10 +59,11 @@ public class ScrControlGame : MonoBehaviour
     }
     public void Web()
     {
-        Application.OpenURL("https://twitter.com/Pamoan10");
+        Application.OpenURL("https://twitter.com/Pamoan10"); //permet accedir a un web 
     }
     public void Sortir()
     {
-        Application.Quit();
+        Application.Quit(); //permet sortir del joc
     }
+    
 }
