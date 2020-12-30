4d275565b8c27ad26f8f0a61c5166089acfbef43
Pamoan10 <moretaap@campusemav.com>
2020-12-31 00:21:47 +0100

    Classe 31/12/2020
    Pokemons mapa 2

--- Assets/Scripts/ScrEnemics.cs
+++ Assets/Scripts/ScrEnemics.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class ScrEnemics : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] int danyPokemon = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<ScrMaya>().Dany(1);
+            collision.gameObject.GetComponent<ScrMaya>().Dany(danyPokemon);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<ScrMaya>().Dany(1);
+            collision.gameObject.GetComponent<ScrMaya>().Dany(danyPokemon);
         }
     }
 
